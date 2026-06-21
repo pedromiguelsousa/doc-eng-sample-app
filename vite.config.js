fdad388f3648164cc76250a0d234ce5f4d117353
pedromiguelsousa <73346792+pedromiguelsousa@users.noreply.github.com>
2026-06-21 18:16:44 +0100

    Add navbar populated from Storyblok content (#5)
    Co-authored-by: Pedro Sousa <pedromiguelsousa@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
-		plugins: [react(), mkcert()],
+		plugins: [react(), mkcert(), tailwindcss()],
 		define: {
 			'import.meta.env.STORYBLOK_DELIVERY_API_TOKEN': JSON.stringify(
 				env.STORYBLOK_DELIVERY_API_TOKEN,
