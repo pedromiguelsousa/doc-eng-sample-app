dc0bde6f617fc38ff6718ef067a23a3b5e0b2b22
Pedro Sousa <pedromiguelsousa@gmail.com>
2026-06-22 19:30:32 +0100

    Add arrow to top-level links in footer

--- src/storyblok/FooterEntry.jsx
+++ src/storyblok/FooterEntry.jsx
@@ -7,7 +7,7 @@ export default function FooterEntry({ blok, topLevelNav = false }) {
     if (topLevelNav) {
         return (
             <div className="mb-4" {...storyblokEditable(blok)}>
-                <p className="text-sm mb-3 font-medium "><a className="underline-offset-4 hover:underline focus:outline-hidden focus:underline" href={url}>{blok.title}</a></p>
+                <p className="text-sm mb-3 font-medium "><a className="underline-offset-4 hover:underline focus:outline-hidden focus:underline" href={url}>▶ {blok.title}</a></p>
             </div>
         );
     } else {
