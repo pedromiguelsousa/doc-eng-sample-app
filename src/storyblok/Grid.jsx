604e40fa2ef63139e5a78062ed7e262b93917ce8
Pedro Sousa <pedromiguelsousa@gmail.com>
2026-06-22 19:16:56 +0100

    Clean up CSS

--- src/storyblok/Grid.jsx
+++ src/storyblok/Grid.jsx
@@ -2,7 +2,7 @@ import { StoryblokComponent, storyblokEditable } from '@storyblok/react';
 
 export default function Grid({ blok }) {
 	return (
-		<div {...storyblokEditable(blok)} className="grid">
+		<div {...storyblokEditable(blok)} className="my-grid">
 			{blok.columns.map((nestedBlok) => (
 				<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
 			))}
